f24ac887dce433a531f721eb97643d2f8819e233
Arthur Sousa <sousaarthur840@gmail.com>
2025-07-27 13:46:18 -0300

    Init development project

---
+++ BeAround/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeAround
+//
+//  Created by Arthur Sousa on 27/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
